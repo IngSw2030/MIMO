70b5d1bfa8a17b7bb65dc4db405510db8b97d95e
Julian Arturo Calle <41351604+ArturoCalle@users.noreply.github.com>
2020-09-18 18:29:39 -0500

    aplicar imagen

--- Mimo-FrontEnd/src/screens/users/HomeScreen.js
+++ Mimo-FrontEnd/src/screens/users/HomeScreen.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
+    const imgSource = require('../../../assets/mimo.png')
     return (
         <View>
             <Text>Pantalla de HomeScreen</Text>
+            <Image style = {styles.imageStyle}  source= {imgSource}/>
+            <Text> hola mundo </Text> 
         </View>
     )
 }
+const styles = StyleSheet.create({
+    imageStyle:{
+        height: 255,
+        width:255
+    }
 
+});
 export default HomeScreen
