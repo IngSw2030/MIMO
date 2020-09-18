e49b9fdd9c2f5e728d345d00d52215695e60615e
Julian Arturo Calle <41351604+ArturoCalle@users.noreply.github.com>
2020-09-18 18:55:16 -0500

    homescreen no sirve aun jeje

--- Mimo-FrontEnd/src/screens/users/HomeScreen.js
+++ Mimo-FrontEnd/src/screens/users/HomeScreen.js
@@ -1,20 +1,62 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
     const imgSource = require('../../../assets/mimo.png')
     return (
         <View>
-            <Text>Pantalla de HomeScreen</Text>
-            <Image style = {styles.imageStyle}  source= {imgSource}/>
-            <Text> hola mundo </Text> 
+            <Image style = {styles.imageStyle}  source= {imgSource} />
+            <Text style={styles.textStyle}> Que buscas hoy? </Text> 
+            <View>
+                <TouchableOpacity >
+                    <View style={styles.viewStyle1}/>
+                </TouchableOpacity>
+                <TouchableOpacity >
+                    <View style={styles.viewStyle2}/>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity >
+                    <View style={styles.viewStyle1}/>
+                </TouchableOpacity>
+                <TouchableOpacity >
+                    <View style={styles.viewStyle2}/>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 const styles = StyleSheet.create({
     imageStyle:{
         height: 255,
-        width:255
+        width:255,
+        alignSelf:'center'
+    },
+    textStyle:{
+        fontSize: 20,
+        fontWeight:'bold',
+        marginLeft:15
+    },
+    viewStyle1:
+    {
+        marginLeft:15,
+        marginTop:15,
+        height:150,
+        width:150,
+        backgroundColor: 'red',
+        borderRadius: 15,
+    },
+    viewStyle2:
+    {
+        marginLeft:15,
+        marginTop:15,
+        height:150,
+        width:150,
+        backgroundColor: 'blue',
+        borderRadius: 15,
+    },
+    generalView:{
+        
     }
 
 });
